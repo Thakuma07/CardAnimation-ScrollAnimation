fc0c6614979360402fa6c126fcba9c546e3c2365
Arkyadeep pal <arkyadeep.pal2017@gmail.com>
2026-02-27 12:48:25 +0530

    feat: add root App component composing HeroSection and MuseumSection

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import MuseumSection from './MuseumSection'
+
+const App = () => {
+  return (
+    <>
+      <HeroSection />
+      <MuseumSection />
+    </>
+  )
+}
+
+export default App
